eaf87ae91fe76336d980b7dd09e502601fee2866
soledadzegarra <zegarrasoledad391@gmail.com>
2025-10-10 20:39:14 -0400

    creacion de labcafeteria sin entidades

---
+++ DDL_DML_CAFETERIA.sql
@@ -0,0 +1,18 @@
+﻿CREATE DATABASE LabCafeteria;
+GO
+USE [master]
+GO
+CREATE LOGIN [usrcafeteria] WITH PASSWORD = N'123456',
+	DEFAULT_DATABASE = [LabCafeteria],
+	CHECK_EXPIRATION = OFF,
+	CHECK_POLICY = ON
+GO
+USE [LabCafeteria]
+GO
+CREATE USER [usrcafeteria] FOR LOGIN [usrcafeteria]
+GO
+ALTER ROLE [db_owner] ADD MEMBER [usrcafeteria]
+GO
+
+
+
